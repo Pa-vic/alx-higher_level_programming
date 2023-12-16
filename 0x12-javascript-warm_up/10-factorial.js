936e9c8db6024ea0cb834e33b2349c4addc7c65f
Pavic <victorndudim2000@gmail.com>
2023-12-16 10:01:33 +0100

    Task 11

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
 let n = 1;
-if (process.argv[2] = '') {
+if (process.argv[2] === 1) {
   console.log('1');
 } else {
-  for (let i = parseInt(process.argv[2]); i > 0 ; i--) {
+  for (let i = parseInt(process.argv[2]); i > 0; i--) {
     n = n * i;
   }
+  console.log(n);
 }
