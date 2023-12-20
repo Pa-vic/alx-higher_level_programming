109634fa3eda94e42224ca86cf3e8f662763feab
Pavic <victorndudim2000@gmail.com>
2023-12-21 00:43:27 +0100

    Task 12

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,14 +1,8 @@
 #!/usr/bin/node
-
-let number;
 if (process.argv.length <= 3) {
   console.log('0');
-} else if (process.argv.length > 2) {
-  number = process.argv[2];
-  for (let i = 3; i <= process.argv.length; i++) {
-    if (process.argv[i] > number) {
-      number = process.argv[i];
-    }
-  }
-  console.log(number);
+} else {
+  const arr = process.argv.slice(2).map(Number);
+  const second = arr.sort(function (a, b) { return b - a; })[1];
+  console.log(second);
 }
